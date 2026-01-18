1450cd36fe9699564e7bad9d24cf2a10ab92cb32
bst-code <bsparksoftwaretacademy@gmail.com>
2026-01-18 09:19:24 +0530

    Python class examples

---
+++ day9/ClassConcept.py
@@ -0,0 +1,31 @@
+class HDFC:
+
+
+    branch = "Nanganallur"
+    Gold_ROI = 10
+
+    def __init__(self):
+        print("Iam Constructor")
+
+    def create_customer(self):
+        print("Creating customer")
+
+    def check_balance(self):
+        print("Checking balance")
+
+    def check_payment(self):
+        print("Checking payment")
+
+    def get_OTP(self):
+        print("Getting OTP")
+
+    @staticmethod
+    def get_aadhar():
+        print("Getting aadhar")
+
+
+obj = HDFC()
+obj.create_customer()
+print(obj.branch)
+
+HDFC.get_aadhar()
